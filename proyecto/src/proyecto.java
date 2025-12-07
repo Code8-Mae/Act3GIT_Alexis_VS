4e4c238e680dd3fccd642adf7ffb91cf41a3910a
Alexis Esteban Martín <aemgame4008@gmail.com>
2025-12-07 18:58:28 +0100

    rama1 creada

--- proyecto/src/proyecto.java
+++ proyecto/src/proyecto.java
@@ -4,5 +4,6 @@ public class proyecto {
             int num = (int)(Math.random() * 10) + 1; 
             System.out.println("Número " + i + ": " + num);
         }
+        System.out.println("Hola lucho, aquí tienes el ejercicio de la rama 1");
     }
 }
